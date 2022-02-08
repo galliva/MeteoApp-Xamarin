3957f155ffb59ada7f1b55e3af77c7ebd72977af
Vanni Galli <galliva@gmail.com>
2022-02-08 12:33:25 +0100

    initial commit

---
+++ MeteoAppSkeleton/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using MeteoAppSkeleton.Views;
+using Xamarin.Forms;
+
+namespace MeteoAppSkeleton
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var nav = new NavigationPage(new MeteoListPage())
+            {
+                BarBackgroundColor = Color.LightGreen,
+                BarTextColor = Color.White
+            };
+
+            MainPage = nav;
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
